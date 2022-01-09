425bdd2d8fa38e46744bfa3cd8b44e895d5165ba
DeathlyBower959 <82238147+DeathlyBower959@users.noreply.github.com>
2022-01-09 16:44:56 -0500

    Update update.py

--- update.py
+++ update.py
@@ -3,23 +3,27 @@ import json
 
 repos = open("repos.json")
 repoData = json.load(repos)
-
 index = 0
 
 for repo in repoData["repos"]:
   if (repo is not None and repo["owner"] is not None and repo["repoName"] is not None): 
-    print(">----------<")
-    print("Fetching latest versions: " + repo["owner"] + "/" + repo["repoName"])
-    try:
-      response = requests.get("https://api.github.com/repos/" + repo["owner"] + "/" + repo["repoName"] + "/releases")
+    owner = repo["owner"]
+    repoName = repo["repoName"]
 
-      with open("release-versions/" + repo["repoName"] + "-latest.txt", "w") as fObj:
-        pprint(str(response.json()), stream=fObj)
-        fObj.close()
-      print("Latest version for: " + repo["owner"] + "/" + repo["repoName"] + " | " + response.json()["tag_name"])
+    print(">----------<")
+    print("Fetching latest versions: " + owner + "/" + repoName)
+    try:
+      res = requests.get("https://api.github.com/repos/" + owner + "/" + repoName + "/releases", headers={"Accept": "application/json"})
+      responseData = json.loads(str("{\"data\": " + res.text + "}"))
+
+      file = open("release-versions/" + repoName + "-latest.json", "w")
+      file.write(json.dumps(responseData, indent=2, sort_keys=True))
+      file.close()
+
+      print("Latest version for: " + owner + "/" + repoName + " | " + responseData["data"][0]["tag_name"])
     except Exception as e: 
-      print("Failed to update latest version for: " + repo["owner"] + "/" + repo["repoName"])
-      print("Reason: ")
+      print("Failed to update latest version for: " + owner + "/" + repoName)
+      print("Reason:", end=" ")
       print(e)
   else:
     print("Failed to update latest version, for repo at index " + str(index) + " because insufficient data was supplied.")
