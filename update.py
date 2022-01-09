165c36eab00c6ab0535ba988650e20683dcba23d
DeathlyBower959 <82238147+DeathlyBower959@users.noreply.github.com>
2022-01-09 11:21:39 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 response = requests.get("https://api.github.com/repos/bozbez/win-capture-audio/releases")
-print(response.json())
+print(json.dumps(json.loads(response.json()), index=4))
