83fa646d8e9eeed82aa7e88abfa97eb6818486c3
DeathlyBower959 <82238147+DeathlyBower959@users.noreply.github.com>
2022-01-09 11:30:39 -0500

    Update update.py

--- update.py
+++ update.py
@@ -5,8 +5,5 @@ repos = open("repos.json")
 repoData = json.load(repos)
 
 for repo in repoData["repos"]:
-  response = requests.get("https://api.github.com/repos/" + repo.owner + "/" + repo.repoName + "/releases")
-
-  file = open("release-versions/" + repo.repoName + ".txt", "w")
-  f.write(response.json())
-  f.close()
+  print(repo)
+  
