cedbcb2ccda7a230fd21cf8370cf9a3f9788d63f
DeathlyBower959 <82238147+DeathlyBower959@users.noreply.github.com>
2022-01-09 11:54:20 -0500

    Update update.py

--- update.py
+++ update.py
@@ -10,14 +10,14 @@ for repo in repoData["repos"]:
   if (repo is not None and repo["owner"] is not None and repo["repoName"] is not None): 
     print("Fetching latest versions: " + repo["owner"] + "/" + repo["repoName"])
     try:
-      response = requests.get("https://api.github.com/repos/" + repo.owner + "/" + repo.repoName + "/releases")
-      file = open("release-versions/" + repo.repoName + ".txt", "w")
+      response = requests.get("https://api.github.com/repos/" + repo["owner"] + "/" + repo["repoName"] + "/releases")
+      file = open("release-versions/" + repo["repoName"] + ".txt", "w")
       f.write(response.json())
       f.close()
       print("Latest version for: " + repo["owner"] + "/" + repo["repoName"] + ", was updated!")
     except Exception as e: 
       print("Failed to update latest version for: " + repo["owner"] + "/" + repo["repoName"])
-      print(e)
+      print("Reason: " + e)
       index += 1
   else:
     print("Failed to update latest version, for repo at index " + str(index) + " because insufficient data was supplied.")
