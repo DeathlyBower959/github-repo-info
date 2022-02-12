50c463cb85772aa2f687f6aa89847275868d4120
DeathlyBower959 <82238147+DeathlyBower959@users.noreply.github.com>
2022-02-12 12:49:03 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,30 +1,79 @@
+import os
 import requests
+import requests.exceptions
 import json
 
 repos = open("repos.json")
 repoData = json.load(repos)
-index = 0
 
-for repo in repoData["repos"]:
-  if (repo is not None and repo["owner"] is not None and repo["repoName"] is not None): 
+alreadyRetrieved = []
+
+
+def isRetrieved(owner, repoName):
+    return {owner, repoName} in alreadyRetrieved
+
+
+for index, repo in enumerate(repoData['repos']):
+    print(">----------<")
+
+    if (repo is None or repo["owner"] is None or repo["repoName"] is None):
+        print(
+            f"Failed to update latest version, for repo at index {str(index)} because insufficient data was supplied.")
+        continue
+
     owner = repo["owner"]
     repoName = repo["repoName"]
 
-    print(">----------<")
-    print("Fetching latest versions: " + owner + "/" + repoName)
+    if (isRetrieved(owner, repoName)):
+        print(
+            f'Skipped fetching versions for {owner}/{repoName} as it was already fetched.')
+        continue
+
+    print(f"Fetching latest versions: {owner}/{repoName}")
     try:
-      res = requests.get("https://api.github.com/repos/" + owner + "/" + repoName + "/releases", headers={"Accept": "application/json"})
-      responseData = json.loads(str("{\"data\": " + res.text + "}"))
+        res = requests.get("https://api.github.com/repos/" + owner + "/" +
+                           repoName + "/releases", headers={"Accept": "application/json"})
+        res.raise_for_status()
+        
+        responseData = json.loads(str("{\"data\": " + res.text + "}"))
+        latestVersion = responseData["data"][0]["tag_name"]
 
-      file = open("release-versions/" + repoName + "-latest.json", "w")
-      file.write(json.dumps(responseData, indent=2, sort_keys=True))
-      file.close()
+        if not os.path.exists(f"./release-versions/{owner}"):
+            os.makedirs(f'./release-versions/{owner}')
 
-      print("Latest version for: " + owner + "/" + repoName + " | " + responseData["data"][0]["tag_name"])
-    except Exception as e: 
-      print("Failed to update latest version for: " + owner + "/" + repoName)
-      print("Reason:", end=" ")
-      print(e)
-  else:
-    print("Failed to update latest version, for repo at index " + str(index) + " because insufficient data was supplied.")
-  index += 1
+        file = open(
+            f"release-versions/{owner}/{repoName}.json", "w")
+        file.write(json.dumps(responseData, indent=2, sort_keys=True))
+        file.close()
+
+        alreadyRetrieved.append({
+            owner,
+            repoName
+        })
+
+        print(f"Latest version for: {owner}/{repoName} | {latestVersion}")
+    except requests.exceptions.HTTPError:
+        print(f'Failed to find repo {owner}/{repoName}')
+    except Exception as e:
+        print(f"Failed to update latest version for: {owner}/{repoName}")
+        print("Reason:", end=" ")
+        print(e)
+
+print(">----------<")
+
+print('Cleaning Up...')
+
+
+def noLongerRequired(owner, repoName):
+    if ({'owner': owner, 'repoName': repoName} not in repoData['repos']):
+        os.remove(f"./release-versions/{owner}/{repoName}.json")
+        print(f'{owner}/{repoName} removed!')
+
+
+for owner in os.listdir('release-versions'):
+    for repo in os.listdir(f'release-versions/{owner}'):
+        noLongerRequired(owner, os.path.splitext(repo)[0])
+
+    if (len(os.listdir(f'./release-versions/{owner}')) == 0):
+        os.rmdir(f'./release-versions/{owner}')
+        print(f'Removed {owner}!')
